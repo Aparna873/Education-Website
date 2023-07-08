ff5611640f3d87e52770ad46b09b5ce65d15a1ea
Aparna873 <appsworld837@gmail.com>
2023-07-08 15:11:00 +0530

    education-website

---
+++ CSS/css/resp.js
@@ -0,0 +1,11 @@
+burgur = document.querySelector('.burgur')
+navbar = document.querySelector('.navbar')
+rightNav = document.querySelector('.rightNav')
+navlist = document.querySelector('.nav-list')
+burgur.addEventListener('click', () => {
+    rightNav.classList.toggle('v-class-resp');
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+
+
+})
